fed98480b334eebd41c1e13d56248a10ce4eb32b
Eric Chiu <EricChiu920@gmail.com>
2019-06-22 17:59:17 -0400

    Initial commit for player class and specs

---
+++ lib/player.rb
@@ -0,0 +1,19 @@
+class Player
+  attr_reader :cards
+
+  def self.deal_player(deck)
+  end
+
+  def initialize(cards)
+  end
+
+  def play_card(pile, card)
+  end
+
+  def draw_from(deck)
+  end
+
+  def play_turn(pile, deck)
+
+  end
+end

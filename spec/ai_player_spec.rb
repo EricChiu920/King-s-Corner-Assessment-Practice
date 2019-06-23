01dc9870304250f5a9e464d72dad22c45d3675d9
IrieY56 <IrieY56@gmail.com>
2019-06-23 00:25:59 -0400

    Add spec for choose_pile and commented out play turn spec since it is still a work in progress

--- spec/ai_player_spec.rb
+++ spec/ai_player_spec.rb
@@ -113,76 +113,91 @@ describe AIPlayer do
     end
   end
 
-  describe '#play_turn' do
-    let(:player) { AIPlayer.new(hand_cards) }
-    let(:pile) { Pile.new(Card.new(:clubs, :three)) }
-    let(:deck) { Deck.new(deck_cards) }
+  describe '#choose_pile' do
+    subject(:player) { AIPlayer.new(cards) }
 
-    context 'with playable card in hand' do
-      let(:hand_cards) { [Card.new(:clubs, :four)] }
-      let(:deck_cards) { [] }
+    let(:pile1) { Pile.new(Card.new(:clubs, :six), Card.new(:clubs, :six)) }
+    let(:pile2) { Pile.new(Card.new(:hearts, :five), Card.new(:hearts, :five)) }
+    let(:piles) { [pile1, pile2] }
+    let(:card) { Card.new(:clubs, :deuce) }
 
-      it 'plays a card out of its hand if possible' do
-        expect(player)
-          .to receive(:play_card)
-          .with(pile, hand_cards[0])
-
-        player.play_turn(pile, deck)
-      end
-    end
-
-    context 'with no playable card in hand' do
-      let(:hand_cards) { [Card.new(:hearts, :four)] }
-
-      let(:deck_cards) do
-        [Card.new(:hearts, :king),
-        Card.new(:hearts, :seven),
-        Card.new(:clubs, :four),
-        Card.new(:hearts, :three)]
-      end
-
-      it 'draws until it takes in a playable card' do
-        expect(player)
-          .to receive(:draw_from)
-          .with(deck)
-          .exactly(3)
-          .times
-          .and_call_original # calls the underlying `Player#draw_from`
-
-        player.play_turn(pile, deck)
-      end
-
-      it 'plays the first drawn playable card' do
-        expect(player)
-          .to receive(:play_card)
-          .with(pile, deck_cards[2])
-
-        player.play_turn(pile, deck)
-      end
-
-      context 'with no playable card in deck' do
-        let(:deck_cards) do
-          [Card.new(:hearts, :king),
-          Card.new(:hearts, :seven)]
-        end
-
-        it 'draws all cards into hand' do
-          expect(player)
-            .to receive(:draw_from)
-            .exactly(2)
-            .times.and_call_original
-
-          player.play_turn(pile, deck)
-        end
-
-        it 'does not play a card' do
-          expect(player).not_to receive(:play_card)
-
-          player.play_turn(pile, deck)
-        end
-      end
+    it 'should move a pile if it can go on top of a another pile' do
+      allow(pile2).to receive(:valid_play?).with(pile1.first_card).and_return(true)
+      expect(player.choose_pile(piles)).to eq([pile1, pile2])
     end
   end
+
+  # work in progress
+  # describe '#play_turn' do
+  #   let(:player) { AIPlayer.new(hand_cards) }
+  #   let(:pile) { Pile.new(Card.new(:clubs, :three)) }
+  #   let(:deck) { Deck.new(deck_cards) }
+
+  #   context 'with playable card in hand' do
+  #     let(:hand_cards) { [Card.new(:clubs, :four)] }
+  #     let(:deck_cards) { [] }
+
+  #     it 'plays a card out of its hand if possible' do
+  #       expect(player)
+  #         .to receive(:play_card)
+  #         .with(pile, hand_cards[0])
+
+  #       player.play_turn(pile, deck)
+  #     end
+  #   end
+
+  #   context 'with no playable card in hand' do
+  #     let(:hand_cards) { [Card.new(:hearts, :four)] }
+
+  #     let(:deck_cards) do
+  #       [Card.new(:hearts, :king),
+  #       Card.new(:hearts, :seven),
+  #       Card.new(:clubs, :four),
+  #       Card.new(:hearts, :three)]
+  #     end
+
+  #     it 'draws until it takes in a playable card' do
+  #       expect(player)
+  #         .to receive(:draw_from)
+  #         .with(deck)
+  #         .exactly(3)
+  #         .times
+  #         .and_call_original # calls the underlying `Player#draw_from`
+
+  #       player.play_turn(pile, deck)
+  #     end
+
+  #     it 'plays the first drawn playable card' do
+  #       expect(player)
+  #         .to receive(:play_card)
+  #         .with(pile, deck_cards[2])
+
+  #       player.play_turn(pile, deck)
+  #     end
+
+  #     context 'with no playable card in deck' do
+  #       let(:deck_cards) do
+  #         [Card.new(:hearts, :king),
+  #         Card.new(:hearts, :seven)]
+  #       end
+
+  #       it 'draws all cards into hand' do
+  #         expect(player)
+  #           .to receive(:draw_from)
+  #           .exactly(2)
+  #           .times.and_call_original
+
+  #         player.play_turn(pile, deck)
+  #       end
+
+  #       it 'does not play a card' do
+  #         expect(player).not_to receive(:play_card)
+
+  #         player.play_turn(pile, deck)
+  #       end
+  #     end
+  #   end
+  # end
 
   describe '#win?' do
     subject(:player) { AIPlayer.new([]) }
